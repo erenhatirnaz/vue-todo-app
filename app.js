1dd4ea099efde2451445b34134877338f491b6a1
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2017-06-07 14:55:32 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+window.addEventListener('load', () => {
+  window.vue = new Vue({
+    el: '#app',
+    data: {
+      todo_list: []
+    },
+
+    mounted() {
+      this.todo_list = this._getTodoList();
+    },
+
+    methods: {
+      /**
+       * @public
+       *
+       * This method gets the to-do item text from textbox and
+       * creates an uncompleated to-do item with this to-do item
+       * text.
+       */
+      addTodoItem() {
+        var $txt_todo_item = this.$refs.txt_todo_item;
+        if (!$txt_todo_item.value) return;
+
+        var todo_item = {
+          status: false,
+          text: $txt_todo_item.value
+        };
+
+        this.todo_list.push(todo_item);
+        this._saveTodoList();
+        $txt_todo_item.value = "";
+      },
+
+      /**
+       * @public
+       *
+       * This method changes to status of to-do item.
+       *
+       * @param {number} index To-do item index
+       */
+      changeStatus(index) {
+        var currentStatus = this.todo_list[index].status;
+        currentStatus = !currentStatus;
+        this._saveTodoList();
+      },
+
+      /**
+       * @public
+       *
+       * This method removes a to-do item from the to-do list.
+       *
+       * @param {number} index To-do item index
+       */
+      removeTodoItem(index) {
+        this.todo_list.splice(index, 1);
+        this._saveTodoList();
+      },
+
+      /**
+       * @private
+       *
+       * This method gets the to-do list from localStorage.
+       *
+       * @returns {Array} To-do List
+       */
+      _getTodoList() {
+        return JSON.parse(localStorage.getItem('todo_list')) || [];
+      },
+
+      /**
+       * @private
+       *
+       * This medhod saves the to-do list as JSON format to localStorage.
+       */
+      _saveTodoList() {
+        localStorage.setItem('todo_list', JSON.stringify(this.todo_list));
+      }
+    }
+  });
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Vue To-Do App
+
+> A very basic to-do list app written with VueJS.
+
+The to-do list is storing in the localStorage.
+
+[Live Demo](https://erenhatirnaz.github.io/vue-todo-app)
+
+## Usage
+
+```sh
+$ git clone https://github.com/erenhatirnaz/vue-todo-app.git
+$ cd vue-todo-app
+$ make serve
+```
+
+and open `localhost:3000` in the your favorite browser.
+
+## Screenshot
+
+![screenshot](screenshot.png)
+
+## License
+
+MIT © [Eren Hatırnaz](https://github.com/ErenHatirnaz)
